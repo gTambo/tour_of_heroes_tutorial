6030882bedb6dad23ba5d93abcd7cc5ce3f9711b
Greg Tambornino <greg.tambornino@concordusa.com>
2023-02-22 14:00:44 -0600

    refactor hero service for asynchronous requests

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 
@@ -8,4 +9,9 @@ import { HEROES } from './mock-heroes';
 export class HeroService {
 
   constructor() { }
+
+  getHeroes(): Observable<Hero[]> {
+    const heroes = of(HEROES);
+    return heroes;
+  }   
 }
